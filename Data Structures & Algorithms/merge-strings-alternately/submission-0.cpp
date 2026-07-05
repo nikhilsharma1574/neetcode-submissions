04fc1bcf0fd2eca71a5451ccad5531964bfa9abd
Nikhil Sharma <79254517+nikhilsharma1574@users.noreply.github.com>
2026-07-05 11:38:56 +0530

    Add: merge-strings-alternately - submission-0

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string ans="";
+        int max_len=max(word1.length(),word2.length());
+        for(int i=0;i<max_len;i++)
+        {
+            if(i<word1.length()) ans+=word1[i];
+            if(i<word2.length()) ans+=word2[i];
+        }
+        return ans;
+    }
+};
